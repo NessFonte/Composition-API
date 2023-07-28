500e871d7cb9fcea675fa34a7f23bcccde081b4b
neisser <neisser2909@gmail.com>
2023-07-28 13:31:14 -0400

    Implementados los Named Slots para reubicar el contenido del Modal

---
+++ src/views/CustomSlots.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Aprendiendo Slots</h1>
+    <button>Abrir Modal</button>
+
+    <modal title="Hola mundo">
+        <template v-slot:footer>
+            <button>Salir</button>
+        </template>
+        <template v-slot:header>
+            <h2>Título del modal</h2>
+        </template>
+        <template v-slot:body>
+            <p>Aliquip ad dolor nostrud aute ad nostrud laborum.</p>
+        </template>
+    </modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal.vue"
+
+export default {
+    components: {Modal},
+
+    setup() {
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
