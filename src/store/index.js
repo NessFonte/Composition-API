df4a1bce0bfd22c5883595274f9019133fea5ba8
neisser <neisser2909@gmail.com>
2023-07-28 11:15:32 -0400

    Implementada la funcionalidad de mostrado de las tareas completadas, pendientes y todas

--- src/store/index.js
+++ src/store/index.js
@@ -18,16 +18,24 @@ export default createStore({
   },
 
   getters: {
-    pendingTodos(state, getters, rootState) {
+    pendingTodos: (state, getters, rootState) => {
       return state.todos.filter(todo => !todo.completed)
     },
 
-    allTodos(state, getters, rootState) {
+    allTodos: (state, getters, rootState) => {
       return state.todos
     },
 
-    completedTodos(state, getters, rootState) {
+    completedTodos: (state, getters, rootState) => {
       return state.todos.filter(todo => todo.completed)
+    },
+
+    getTodosByTab: (state, getters, rootState) => (tab) => {
+      switch(tab) {
+        case 'all': return getters.allTodos
+        case 'pending': return getters.pendingTodos
+        case 'completed': return getters.completedTodos
+      }
     }
   },
 
--- src/views/TodoVuex.vue
+++ src/views/TodoVuex.vue
@@ -2,29 +2,81 @@
     <h1>Lista de tareas de Thanos</h1>
     <h4>Pendientes: {{ pending.length }}</h4>
     <hr>
-    <h4>Todas: {{ all.length }}</h4>
-    <h4>Completadas: {{ completed.length }}</h4>
+    <button 
+    :class="{'active': currentTab === 'all'}"
+    @click="currentTab = 'all'"    
+    >Todos</button>
+    <button 
+    :class="{'active': currentTab === 'pending'}"
+    @click="currentTab = 'pending'"
+    >Pendientes</button>
+    <button 
+    :class="{'active': currentTab === 'completed'}"
+    @click="currentTab = 'completed'"
+    >Completados</button>
+
+    <div>
+        <ul>
+            <li v-for="todo in getTodosByTab" :key="todo.id" :class="{'completed': todo.completed}">{{ todo.text }}</li>
+        </ul>
+    </div>
 
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
     setup() {
         const store = useStore()
+        const currentTab = ref('all')
         
         return {
+            currentTab,
+
             pending: computed(() => store.getters['pendingTodos']),
             all: computed(() => store.getters['allTodos']),
             completed: computed(() => store.getters['completedTodos']),
+            getTodosByTab: computed(() => store.getters['getTodosByTab'](currentTab.value)),
         }
     }
 
 }
 </script>
 
-<style>
+<style scoped>
+div {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    margin-left: 100px;
+}
 
+ul {
+    width: 350px;
+    text-align: left;
+}
+
+li {
+    cursor: pointer;
+}
+
+button {
+    width: 100px;
+    height: 25px;
+    margin: 2px;
+    cursor: pointer;
+    border-radius: 5px;
+    border: none;
+}
+
+.active {
+    background-color: #2c3e50;
+    color: white;
+}
+
+.completed {
+    text-decoration: line-through;
+}
 </style>
